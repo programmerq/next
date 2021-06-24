db59fdd9bf32adb65d0aad8e9b637696a3ae0e6d
Alexey Ivanov <mail@alexeyivanov.info>
2021-06-24 20:26:42 +0900

    Add fallback local url to marketo

--- utils/marketo.ts
+++ utils/marketo.ts
@@ -1,22 +1,28 @@
 import { useState, useEffect } from "react";
 
+const { NEXT_PUBLIC_HOST } = process.env;
 const { NEXT_PUBLIC_MARKTO_MUNCHKIN_ID } = process.env;
 const { NEXT_PUBLIC_MARKTO_BASE_URL } = process.env;
 
-function loadScript(): Promise<boolean> {
-  return new Promise((resolve) => {
-    if (window.MktoForms2) {
-      return resolve(true);
+function loadScript(baseUrl: string): Promise<string> {
+  const url = `${baseUrl}/js/forms2/js/forms2.min.js`;
+
+  return new Promise((resolve, reject) => {
+    if (window.MktoForms2 && document.querySelector(`script[src=${url}]`)) {
+      return resolve(baseUrl);
     }
 
     const scriptElement = document.createElement("script");
 
-    scriptElement.src = `${NEXT_PUBLIC_MARKTO_BASE_URL}/js/forms2/js/forms2.min.js`;
     scriptElement.onload = () => {
       if (window.MktoForms2) {
-        return resolve(true);
+        return resolve(baseUrl);
       }
     };
+
+    scriptElement.onerror = () => reject(new Error("Can't load script"));
+
+    scriptElement.src = `${baseUrl}/js/forms2/js/forms2.min.js`;
 
     document.body.appendChild(scriptElement);
   });
@@ -28,21 +34,25 @@ interface UseMarketoProps {
 }
 
 export function useMarketo({ formId, callback }: UseMarketoProps): boolean {
-  const [loaded, setLoaded] = useState(false);
+  const [url, setUrl] = useState<string>(undefined);
 
   useEffect(() => {
-    if (!loaded) {
-      loadScript().then(setLoaded);
+    if (!url) {
+      loadScript(NEXT_PUBLIC_MARKTO_BASE_URL)
+        .catch(() => loadScript(NEXT_PUBLIC_HOST))
+        .then(setUrl)
+        .catch((e) => console.error(e));
+
       return;
     }
 
     window.MktoForms2.loadForm(
-      NEXT_PUBLIC_MARKTO_BASE_URL,
+      url,
       NEXT_PUBLIC_MARKTO_MUNCHKIN_ID,
       formId,
       callback
     );
-  }, [loaded, formId, callback]);
+  }, [url, formId, callback]);
 
-  return loaded;
+  return Boolean(url);
 }
